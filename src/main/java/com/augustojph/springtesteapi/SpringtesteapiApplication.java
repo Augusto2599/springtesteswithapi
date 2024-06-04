029a7b9897a2495427d24b7eb0e6f6c443e854ce
José Augusto <Augusto2599@outlook.com>
2024-06-04 17:34:18 -0300

    Criando uma api com teste automatizado

---
+++ src/main/java/com/augustojph/springtesteapi/SpringtesteapiApplication.java
@@ -0,0 +1,13 @@
+package com.augustojph.springtesteapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtesteapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringtesteapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/augustojph/springtesteapi/SpringtesteapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.augustojph.springtesteapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtesteapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
