829bd50000900b34bb67315942c3bb0dc516f43c
José Augusto <Augusto2599@outlook.com>
2024-06-05 14:26:14 -0300

    Criando o teste de consulta por Nome.

--- src/main/java/com/augustojph/springtesteapi/domain/PlanetRepository.java
+++ src/main/java/com/augustojph/springtesteapi/domain/PlanetRepository.java
@@ -1,7 +1,9 @@
 package com.augustojph.springtesteapi.domain;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface PlanetRepository extends CrudRepository<Planet, Long> {
-
+    Optional<Planet> findByName(String name);
 }
