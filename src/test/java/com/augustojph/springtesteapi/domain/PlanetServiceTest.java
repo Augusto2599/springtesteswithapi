1a3b7a39799fcebaf142a3b9ed7e5bc8f1b54624
José Augusto <Augusto2599@outlook.com>
2024-06-05 13:50:51 -0300

    Criando o teste de consulta por Id.

--- src/test/java/com/augustojph/springtesteapi/domain/PlanetServiceTest.java
+++ src/test/java/com/augustojph/springtesteapi/domain/PlanetServiceTest.java
@@ -4,7 +4,10 @@ import static com.augustojph.springtesteapi.common.PlanetConstants.INVALID_PLANE
 import static com.augustojph.springtesteapi.common.PlanetConstants.PLANET;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
+
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -40,6 +43,25 @@ public class PlanetServiceTest {
     public void createPlanet_WithInvalidData_ThrowsException() {
         when(planetRepository.save(INVALID_PLANET)).thenThrow(RuntimeException.class);
 
-       assertThatThrownBy(() -> planetService.create(INVALID_PLANET)).isInstanceOf(RuntimeException.class);
+        assertThatThrownBy(() -> planetService.create(INVALID_PLANET)).isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    public void getPlanet_ByExistingId_ReturnsPlanet() {
+        when(planetRepository.findById(anyLong())).thenReturn(Optional.of(PLANET));
+
+        Optional<Planet> sut = planetService.get(1L);
+
+        assertThat(sut).isNotEmpty();
+        assertThat(sut.get()).isEqualTo(PLANET);
+    }
+
+    @Test
+    public void getPlanet_ByUnexistingId_ReturnsEnpty() {
+        when(planetRepository.findById(anyLong())).thenReturn(Optional.empty());
+
+        Optional<Planet> sut = planetService.get(1L);
+
+        assertThat(sut).isEmpty();
     }
 }
