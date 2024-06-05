d8a4797903e7b8ff1bd9cba0415875e37cc5be55
José Augusto <Augusto2599@outlook.com>
2024-06-05 12:47:20 -0300

    Configurando o cenario de teste para criação de dados.

--- src/test/java/com/augustojph/springtesteapi/domain/PlanetServiceTest.java
+++ src/test/java/com/augustojph/springtesteapi/domain/PlanetServiceTest.java
@@ -2,20 +2,33 @@ package com.augustojph.springtesteapi.domain;
 
 import static com.augustojph.springtesteapi.common.PlanetConstants.PLANET;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.test.mock.mockito.MockBean;
 
-@SpringBootTest(classes = PlanetService.class)
+//@SpringBootTest(classes = PlanetService.class)
+@ExtendWith(MockitoExtension.class)
 public class PlanetServiceTest {
     
-    @Autowired
+    //@Autowired
+    @InjectMocks
     private PlanetService planetService;
 
+    //@MockBean
+    @Mock
+    private PlanetRepository planetRepository;
 
     @Test
     public void createdData_WithValidData_ReturnsPlanet() {
+        when(planetRepository.save(PLANET)).thenReturn(PLANET);
+
         Planet sut = planetService.create(PLANET);
 
         assertThat(sut).isEqualTo(PLANET);
