55fc970ddf2d1ddc9849e48b3f51afded35faa6b
José Augusto <Augusto2599@outlook.com>
2024-06-10 15:49:38 -0300

    Testando um possivel cenário de sucessso no repository.

---
+++ src/test/java/com/augustojph/springtesteapi/domain/PlanetRepositoryTest.java
@@ -0,0 +1,35 @@
+package com.augustojph.springtesteapi.domain;
+
+import static com.augustojph.springtesteapi.common.PlanetConstants.PLANET;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+// import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+// @SpringBootTest(classes = PlanetRepository.class)
+@DataJpaTest
+public class PlanetRepositoryTest {
+
+    @Autowired
+    private PlanetRepository planetRepository;
+
+    @Autowired
+    private TestEntityManager testEntityManager;
+
+    @Test
+    public void createPlanet_WithValidData_RetunrsPlanet() {
+        Planet planet = planetRepository.save(PLANET);
+
+        Planet sut = testEntityManager.find(Planet.class, planet.getId());
+
+        System.out.println(planet);
+
+        assertThat(sut).isNotNull();
+        assertThat(sut.getName()).isEqualTo(PLANET.getName());
+        assertThat(sut.getClimate()).isEqualTo(PLANET.getClimate());
+        assertThat(sut.getTerrain()).isEqualTo(PLANET.getTerrain());
+    }
+}
